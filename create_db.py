62235a08deab3b87cd2009ee5d0beefab2c957b6
gao157 <38510925+gao157@users.noreply.github.com>
2019-02-19 14:14:04 -0800

    Add files via upload

--- create_db.py
+++ create_db.py
@@ -8,14 +8,12 @@ creates a database from csv file
 filename = 'transaction.csv'
 _chunksize = 50000
 
-#lines = subprocess.check_output('cat ' + filename + ' | wc -l', shell=True)
-#lines = int(lines.decode())
-lines = 10000000
+lines = subprocess.check_output('cat ' + filename + ' | wc -l', shell=True)
+lines = int(lines.decode())
 
 reader = pd.read_csv(filename, chunksize=_chunksize, low_memory=False)
 chunk = next(reader)
-#sqlite_filename = filename.split('.')[0]
-sqlite_filename = 'small'
+sqlite_filename = filename.split('.')[0]
 _conn = sqla.create_engine('sqlite:///' + sqlite_filename)
 chunk.to_sql(sqlite_filename, _conn, if_exists='replace')
 
@@ -24,5 +22,3 @@ for chunk in reader:
 	count += _chunksize
 	print('appending... ', str(count) + '/' + str(lines), '{0:.2f}%'.format((count / lines) * 100))
 	chunk.to_sql(sqlite_filename, _conn, if_exists='append')
-	if count > 1000:
-		break
